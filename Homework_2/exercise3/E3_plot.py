b0d1564a56a27f1855cb0c9b1e41e1fdd3ff1f05
Ilaria Carannante <ilariac@kth.se>
2020-09-25 17:24:14 +0200

    figure finished

--- Homework_2/exercise3/E3_plot.py
+++ Homework_2/exercise3/E3_plot.py
@@ -8,8 +8,12 @@ y_stream_avg=np.loadtxt('summary_critical_avg.txt', unpack= True)
 y_stream_std=np.loadtxt('summary_critical_std.txt', unpack= True)
 yy_stream_avg=np.loadtxt('summary_noncrit_avg.txt', unpack= True)
 yy_stream_std=np.loadtxt('summary_noncrit_std.txt', unpack= True)
+yyy_stream_avg=np.loadtxt('summary_q4_avg.txt', unpack= True)
+yyy_stream_std=np.loadtxt('summary_q4_std.txt', unpack= True)
+yyyy_stream_avg=np.loadtxt('summary_padding_avg.txt', unpack= True)
+yyyy_stream_std=np.loadtxt('summary_padding_std.txt', unpack= True)
 
-plt.figure()
+plt.figure(1)
 plt.hlines(0.001721, 1, 32, color='blueviolet', label ='serial')
 plt.plot(x_stream_avg,y_stream_avg,'--', label='critical')
 plt.errorbar(x_stream_avg, y_stream_avg, yerr=y_stream_std,fmt='rs',capsize=5,markersize=3,elinewidth=2,ecolor='k')
@@ -24,7 +28,7 @@ plt.legend()
 plt.grid()
 plt.savefig('Ex3_Q3_1.png')
 
-plt.figure()
+plt.figure(2)
 plt.hlines(0.001721, 1, 32, color='blueviolet', label ='serial')
 plt.semilogy(x_stream_avg,(y_stream_avg),'--', label='critical')
 plt.errorbar(x_stream_avg, y_stream_avg, yerr=y_stream_std,fmt='rs',capsize=5,markersize=3,elinewidth=2,ecolor='k')
@@ -40,20 +44,54 @@ plt.grid()
 plt.savefig('Ex3_Q3_1_semilogy.png')
 
 
-'''
-import pandas as pd
+plt.figure(3)
+plt.hlines(0.001721, 1, 32, color='blueviolet', label ='serial')
+plt.plot(x_stream_avg,yyy_stream_avg,'--',color='orange', label='local max')
+plt.errorbar(x_stream_avg, yyy_stream_avg, yerr=yyy_stream_std,fmt='rs',capsize=5,markersize=3,elinewidth=2,ecolor='k')
+plt.xlabel('# of threads')
+plt.ylabel('mean time (s)')
+plt.title('Ex 3 - Q3_4')
+plt.legend()
+plt.grid()
+plt.savefig('Ex3_Q3_4a.png')
 
-#df = pd.read_json('{"Schedules":{"0":"Static","1":"Dynamic","2":"Guided"},"count":{"0":83223.24,"1":225.74,"2":53504.3}}')
-#df.plot(x='Schedules', y='count', kind='bar', legend=False)
+plt.figure(4)
+plt.hlines(0.001721, 1, 32, color='blueviolet', label ='serial')
+plt.semilogy(x_stream_avg,(y_stream_avg),'--', label='critical')
+plt.errorbar(x_stream_avg, y_stream_avg, yerr=y_stream_std,fmt='rs',capsize=5,markersize=3,elinewidth=2,ecolor='k')
+plt.plot(x_stream_avg,yyy_stream_avg,'--',color='orange', label='local max')
+plt.errorbar(x_stream_avg, yyy_stream_avg, yerr=yyy_stream_std,fmt='rs',capsize=5,markersize=3,elinewidth=2,ecolor='k')
 
-df = pd.read_json('{"Schedules":{"0":"Static","1":"Dynamic","2":"Guided"},"count":{"0": 4.920 ,"1":2.353,"2":4.728}}')
+plt.semilogy(x_stream_avg,yy_stream_avg,'--g', label='non critical')
+plt.errorbar(x_stream_avg, yy_stream_avg, yerr=yy_stream_std,fmt='rs',capsize=5,markersize=3,elinewidth=2,ecolor='k')
+
+plt.xlabel('# of threads')
+plt.ylabel('mean time (s)')
+plt.title('Ex 3 - Q3_5 - semilogy')
+plt.legend()
+plt.grid()
+plt.savefig('Ex3_Q3_4b.png')
+
+plt.figure(5)
+plt.hlines(0.001721, 1, 32, color='blueviolet', label ='serial')
+plt.semilogy(x_stream_avg,(y_stream_avg),'--', label='critical')
+plt.errorbar(x_stream_avg, y_stream_avg, yerr=y_stream_std,fmt='rs',capsize=5,markersize=3,elinewidth=2,ecolor='k')
+plt.plot(x_stream_avg,yyy_stream_avg,'--',color='orange', label='local max')
+plt.errorbar(x_stream_avg, yyy_stream_avg, yerr=yyy_stream_std,fmt='rs',capsize=5,markersize=3,elinewidth=2,ecolor='k')
+
+plt.plot(x_stream_avg,yyyy_stream_avg,'--',color='black', label='padding')
+plt.errorbar(x_stream_avg, yyyy_stream_avg, yerr=yyyy_stream_std,fmt='rs',capsize=5,markersize=3,elinewidth=2,ecolor='k')
 
 
-df.plot(x='Schedules', y='count', kind='bar', legend=False)
-plt.ylabel('Log')
-plt.title('Ex 3 - Q1.3') 
-plt.savefig('Ex2_Q1_3.png')
-'''
+plt.semilogy(x_stream_avg,yy_stream_avg,'--g', label='non critical')
+plt.errorbar(x_stream_avg, yy_stream_avg, yerr=yy_stream_std,fmt='rs',capsize=5,markersize=3,elinewidth=2,ecolor='k')
+
+plt.xlabel('# of threads')
+plt.ylabel('mean time (s)')
+plt.title('Ex 3 - Q3_5 - semilogy')
+plt.legend()
+plt.grid()
+plt.savefig('Ex3_Q3_5.png')
 
 
 plt.show()
